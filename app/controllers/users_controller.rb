d0b98185b2d32368c935e425a1b7b58b7ee795ff
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-04-21 10:04:48 -0400

    thursday commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,44 @@
 class UsersController < ApplicationController
-    def index
-        users = User.all
-        render json: UserSerializer.new(users)
-    end
-    
-    def create
-        user = User.new(username: param['username'], password: params['password'])
-        if user.save
-            payload = {'user_id': user.id}
-            token = encode(payload)
-            render json: {
-                user: UserSerializer.new(user),
-                token: token,
-                authenticated: true
-            }
-        else 
-            render json: { message: 'There was an error creating your account' }
-        end
-    end
+  def index
+    users = User.all
+    render json: UserSerializer.new(users)
+  end
 
-    def show
-        token = request.headers['Authentication'].split(' ')[1]
-        payload = decode(token)
-        user = User.find(payload['user_id'])
-        if user
-            render json: UserSerializer.new(user)
-        else 
-            render json: { message: 'Error', authenticated: false }
-        end
+  def create
+    user = User.create(user_params)
+    if user
+      payload = { 'user_id': user.id }
+      token = encode(payload)
+      render json: {
+               user: UserSerializer.new(user),
+               token: token,
+               authenticated: true,
+             }
+    else
+      render json: { message: 'There was an error creating your account' }
     end
+  end
 
-    def update
-        user = User.find(params[:id])
-        user.update(online: params[:online]);
-        ActionCable.server.broadcast 'appearance_channel', json_response(user)
+  def show
+    token = request.headers['Authentication'].split(' ')[1]
+    payload = decode(token)
+    user = User.find(payload['user_id'])
+    if user
+      render json: UserSerializer.new(user)
+    else
+      render json: { message: 'Error', authenticated: false }
     end
+  end
 
-    private
+  def update
+    # user = User.find(params[:id])
+    # user.update(online: params[:online]);
+    # ActionCable.server.broadcast 'appearance_channel', json_response(user)
+  end
 
-    def user_params
-        params.require(:user).permit(:username, :password, :avatar)
-    end
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
